8ebf94e61b39941e6d9b13d1430ff1f569c1b071
Valenstain <svs.laboratory@gmail.com>
2024-10-06 10:55:58 +0300

    Created a project

---
+++ src/main/java/com/solodkov/Homebank/HomebankApplication.java
@@ -0,0 +1,13 @@
+package com.solodkov.Homebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankApplication.class, args);
+	}
+
+}
